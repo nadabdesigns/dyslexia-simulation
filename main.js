7ad9f0fe3c3b2fac83e51445e955c979329eab84
Nadia Batchelor <nadabdesigns@gmail.com>
2025-03-31 12:42:26 -0400

    Dyslexia Simulation staring

---
+++ main.js
@@ -0,0 +1,10 @@
+// done document query slector 
+// passed in ID
+// property on click 
+// assing printed out click
+
+document.querySelector ('#hide-visited').onclick = () => {
+	console.log('click')
+	docutment.querySelector('#pizza-places').classList.add('hidevisited')
+}
+
