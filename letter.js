8f78bb84f98f4039f76cbecf03f0deb14a25f839
Nadia Batchelor <nadabdesigns@gmail.com>
2025-04-10 12:52:06 -0400

    aded in matter.js

--- letter.js
+++ letter.js
@@ -1,4 +1,43 @@
 // require https://cdn.jsdelivr.net/npm/p5@latest/lib/p5.min.js
+// 
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+	// creat engine
+	var engine = Engine.create();
+
+	// create a renderer and asigning it to body
+var render = Render.create({
+    element: document.body,
+    engine: engine
+});
+
+// create two boxes and a ground first x then y then width and height
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var boxC = Bodies.rectangle(450, 50, 80, 80);
+var boxD = Bodies.rectangle(450, 50, 80, 80);
+var boxE = Bodies.rectangle(450, 50, 80, 80);
+
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB,boxC, ground]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
+
+
 
 const gravity = 1; // accelerate 1 pixel per frame
 let velocity = 0;
